8d222f3df64af007179356b2eebbe5b51c2f9d5e
KEENTAA <ramiro.quenta@ucb.edu.bo>
2025-04-05 00:24:00 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/We/', // ⚠️ Case-sensitive (debe coincidir con tu repo)
   plugins: [react()],
-  base: '/We/',
-})
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
+  }
+});
